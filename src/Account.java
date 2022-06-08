f41459423615dfde56e3304f72b8e335b04a089c
Alvian Nur Ramadhani <alviannur.ramadhani@mitrais.com>
2022-06-08 14:35:47 +0700

    add welcome screen and validate input

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,60 @@
+import java.util.Objects;
+
 public class Account {
+    private String name;
+    private String pin;
+    private double balance;
+    private String accountNumber;
 
+    public Account(String name, String pin, double balance, String accountNumber){
+        this.name = name;
+        this.pin = pin;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return pin == account.pin && Double.compare(account.balance, balance) == 0 && accountNumber == account.accountNumber && Objects.equals(name, account.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, pin, balance, accountNumber);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        /*
+        * ATM record account
+        * */
+        var john = new Account("John Doe", "012108", 100, "112233");
+        var jane = new Account("Jane Doe", "932012", 30, "112244");
+        var accounts = new ArrayList<>(Arrays.asList(john,jane));
+        Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.println("=== ATM Welcome Screen === ");
+            System.out.println("Enter Account Number: ");
+            var accNum = scanner.nextLine();
+            System.out.println("Enter PIN: ");
+            var pin = scanner.nextLine();
+
+            // Validate account
+            var authenticatedAcc = validateLogin(accounts,accNum,pin);
+
+        }
+
+    }
+    public static Account validateLogin(List<Account> accounts ,String accNum, String pin){
+        if(accNum.length() != 6) {
+            System.out.println("Account Number should have 6 digits length");
+            return null;
+        }
+        // regex for string only contains number -> "[0-9]+" or "\\d+"
+        if(!accNum.matches("\\d+")){
+            System.out.println("Account Number should only contains numbers [0-9]");
+            return null;
+        }
+        if(pin.length() != 6){
+            System.out.println("PIN should have 6 digits length");
+            return null;
+        }
+        if(!pin.matches("\\d+")){
+            System.out.println("PIN should only contains numbers [0-9]");
+            return null;
+        }
+        if(Main.IsExist(accounts,accNum,pin)){
+            return accounts.stream()
+                    .filter(a -> a.getAccountNumber().equals(accNum) && a.getPin().equals(pin))
+                    .findAny().get();
+        }else {
+            System.out.println("Invalid Account Number/PIN");
+            return null;
+        }
+
+    }
+
+    public static boolean IsExist(List<Account> accounts,String accNum, String pin){
+        return accounts.stream().anyMatch(
+                acc -> acc.getAccountNumber().equals(accNum) && acc.getPin().equals(pin));
     }
 }
